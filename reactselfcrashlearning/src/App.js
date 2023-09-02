c556204a1a794c454f7a2512bc6756068321734c
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-02 13:35:38 -0300

    Cleanup for learning

--- reactselfcrashlearning/src/App.js
+++ reactselfcrashlearning/src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import React, { } from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import Counter from './Counter'
+import React from 'react';
 
 // This component is a header
 function Header() {
@@ -32,9 +29,7 @@ function Footer() {
 function Content() {
   return (
     <>
-      <Provider store={store}>
-        <Counter />  
-      </Provider>
+      Hello World!
     </>
   );
 }
--- reactselfcrashlearning/src/ChildComponent.js
+++
@@ -1,9 +0,0 @@
-function ChildComponent({itemList}) {
-    return (
-        <ul>
-            {itemList.map((item) => <li key={item}>{item}</li>)}
-        </ul>
-    );
-}
-
-export default ChildComponent;
