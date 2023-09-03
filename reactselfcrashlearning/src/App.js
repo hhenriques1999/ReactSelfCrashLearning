5c3bc7a7b036d1faaa3b2e2101b41dd41e22caa6
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-03 00:30:06 -0300

    Cleanup

--- reactselfcrashlearning/src/App.js
+++ reactselfcrashlearning/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import ControlledInput from './ControlledInput';
-import MultipleControlledInputs from "./MultipleControlledInputs";
-
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
   return (
-    <div>
-      <h1>Controlled Component</h1>
-      <ControlledInput />
-      <MultipleControlledInputs/>
-    </div>
+    <>
+    </>
   );
 }
 
