844c518ea804ac57c6fd11dcef197e8086766f98
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-02 21:39:49 -0300

    Cleanup

--- reactselfcrashlearning/src/App.js
+++ reactselfcrashlearning/src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
-let debounceTimer;
-
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
-  const [query, setQuery] = useState('');
-
-  const handleSearch = (e) => {
-    const searchText = e.target.value;
-
-    clearTimeout(debounceTimer);
-
-    debounceTimer = setTimeout(() => {
-      setQuery(searchText);
-    }, 300);
-  }
-  
   return (
-    <input type="text" placeholder='Search...' onChange={handleSearch}></input>
+    <>
+    </>
   );
 }
 
