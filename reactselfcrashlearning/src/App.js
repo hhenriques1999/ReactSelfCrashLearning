def9b1a8c98cf226386a60a2858eaa80cfdd47fe
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-02 21:18:17 -0300

    Cleanup

--- reactselfcrashlearning/src/App.js
+++ reactselfcrashlearning/src/App.js
@@ -1,40 +1,10 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
-const names = ['Alice', 'Bob', 'Charlie'];
-
-const people = [
-  { id: '1', name: 'Alice' },
-  { id: '2', name: 'Bob' },
-  { id: '3', name: 'Charlie' }
-];
-
-function PeopleList() {
-  return (
-    <ul>
-      {people.map(person => (
-        <li key={person.id}>{person.name}</li>
-      ))}
-    </ul>
-  )
-}
-
-function NameList() {
-  return  (
-    <ul>
-      {names.map((name, index) => (
-        <li key={index}>{name}</li>
-      ))}
-    </ul>
-  );
-}
-
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
   return (
     <>
-    <NameList/>
-    <PeopleList/>
     </>
   );
 }
