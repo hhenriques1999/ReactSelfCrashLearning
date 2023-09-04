309dbfe54961e7af7dcf0c63fe050c0a81dcc1d3
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-03 22:37:46 -0300

    Cleanup

--- reactselfcrashlearning/src/App.js
+++ reactselfcrashlearning/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import React, { useEffect, useState, Suspense } from 'react';
-
-const MyComponent = React.lazy(() => import('./MyComponent'));
+import React, { useEffect, useState } from 'react';
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <MyComponent></MyComponent>
-      </Suspense>
-    </div>
+    <>
+    </>
   );
 }
 
--- reactselfcrashlearning/src/MyComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function MyComponent() {
-    return (
-        <div>Hello, I'm lazy! Or suspenseful...</div>
-    )
-}
-
-export default MyComponent;
