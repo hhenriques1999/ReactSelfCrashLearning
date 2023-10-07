c78ad5e798c5ec92880400ab513eabced6166f92
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-07 19:43:48 -0300

    Remove unused declarations and code

--- reactselfcrashlearning/src/App.jsx
+++ reactselfcrashlearning/src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
   return (
-    <div className='mt-2' style={{width: "25%"}}>
-      <select className='form-control' name="test" id="test">
-        <option value="1">1</option>
-        <option value="2">2</option>
-      </select>
-    </div>
+	<>
+	</>
   );
 }
 
