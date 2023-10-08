a1789219507c701fd5c65a391873792e56dc1999
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-07 22:07:59 -0300

    Component rendering unit testing

--- reactselfcrashlearning/src/App.jsx
+++ reactselfcrashlearning/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
+import FirstTest from './FirstTest';
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
-  return (
-	<>
-	</>
-  );
+	return (
+		<>
+			<FirstTest></FirstTest>
+		</>
+	);
 }
 
 export default App;
