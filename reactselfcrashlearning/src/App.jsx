ac1878ff493d628eb480bc68017283dbf6031877
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-07 21:47:57 -0300

    Cleanup

--- reactselfcrashlearning/src/App.jsx
+++ reactselfcrashlearning/src/App.jsx
@@ -1,23 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import React from 'react';
-import { Provider } from 'react-redux';
 import './App.css';
-import Counter from './Counter';
-import counterReducer from './reducers';
-
-const store = configureStore({
-	reducer: {
-		counter: counterReducer,
-	},
-});
+import React from 'react';
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
-	return (
-		<Provider store={store}>
-			<Counter />
-		</Provider>
-	);
+  return (
+	<>
+	</>
+  );
 }
 
 export default App;
