db74e4a4240fc5ad191067b8195d47b85de09c90
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-07 21:47:08 -0300

    Redux Toolkit for State Management

--- reactselfcrashlearning/src/App.jsx
+++ reactselfcrashlearning/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css';
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
+import { Provider } from 'react-redux';
+import './App.css';
+import Counter from './Counter';
+import counterReducer from './reducers';
+
+const store = configureStore({
+	reducer: {
+		counter: counterReducer,
+	},
+});
 
 // This component is the main app's component and should really only include the header, footer and content (child components)
 function App() {
-  return (
-	<>
-	</>
-  );
+	return (
+		<Provider store={store}>
+			<Counter />
+		</Provider>
+	);
 }
 
 export default App;
