4fbc5146793a4b307cb86e3fa9875022b5ade4cd
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-07 23:28:14 -0300

    Cover click handler case

--- reactselfcrashlearning/src/TestWithMockData.test.jsx
+++ reactselfcrashlearning/src/TestWithMockData.test.jsx
@@ -1,6 +1,6 @@
-import TestWithMockData from "./TestWithMockData"
-import { render, screen } from '@testing-library/react'
-
+import TestWithMockData from "./TestWithMockData";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const mockData = [
     {
@@ -15,21 +15,32 @@ const mockData = [
         "last_name": "Harrild",
         "email": "charrild1@dion.ne.jp",
         "age": 35
-    },{
+    }, {
         "id": 3,
         "first_name": "Amby",
         "last_name": "Emmer",
         "email": "aemmer2@buzzfeed.com",
         "age": 67
     },
-]
+];
 
 test("List renders successfully", () => {
-    render(<TestWithMockData data={mockData} displayUnorderedList={true}/>);
+    render(<TestWithMockData data={mockData} displayUnorderedList={true} />);
     expect(screen.getByText(/fletcher/i)).toBeInTheDocument();
-})
+});
 
 test("Ordered list renders", () => {
-    render(<TestWithMockData data={mockData} displayUnorderedList={false} />)
+    render(<TestWithMockData data={mockData} displayUnorderedList={false} />);
     expect(screen.getByText(/McVanamy/i)).toBeInTheDocument();
-})
+});
+
+test("Email link click handler called", async () => {
+    const mockHandleClick = jest.fn();
+    render(<TestWithMockData 
+                data={mockData} 
+                displayUnorderedList={true}
+                handleClick = {mockHandleClick}
+          />);
+    await userEvent.click(screen.getByText(/mmcvanamy0@e-recht24.de/i));
+    expect(mockHandleClick).toHaveBeenCalled();
+});
